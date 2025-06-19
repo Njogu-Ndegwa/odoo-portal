eb41114fd6f2f0947924412cb557efea4b16c707
Dennis <dennisndegwa001@gmail.com>
2025-06-20 02:46:20 +0300

    update Url

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://dev-federated-graphql-api.omnivoltaic.com/graphql",
+  uri: "https://federated-graphql-api.omnivoltaic.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
